sushiswap/sushiswap-interface

feat: extend farm details panel

src/features/onsen/KashiDeposit.tsx
@@ -1,0 +1,138 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { BENTOBOX_ADDRESS, CurrencyAmount, WNATIVE } from '@sushiswap/sdk'
+import React, { useState } from 'react'
+import Button, { ButtonError } from '../../components/Button'
+import Dots from '../../components/Dots'
+import Web3Connect from '../../components/Web3Connect'
+import { KashiCooker } from '../../entities'
+import { ZERO } from '../../functions/math'
+import { ApprovalState, useActiveWeb3React, useApproveCallback } from '../../hooks'
+import { useCurrency } from '../../hooks/Tokens'
+import useKashiApproveCallback, { BentoApprovalState } from '../../hooks/useKashiApproveCallback'
+import CurrencyInputPanel from './CurrencyInputPanel'
+import Provider, { useKashiInfo } from '../kashi/context'
+import { tryParseAmount } from '../../functions'
+import Alert from '../../components/Alert'
+import { useUSDCValue } from '../../hooks/useUSDCPrice'
+import { BigNumber } from '@ethersproject/bignumber'
+
+const KashiDeposit = ({ pair, useBento }) => {
+  const { i18n } = useLingui()
+  const { account, chainId } = useActiveWeb3React()
+
+  const assetToken = useCurrency(pair?.asset.address) || undefined
+  const info = useKashiInfo()
+
+  const [depositValue, setDepositValue] = useState('')
+
+  const assetNative = WNATIVE[chainId || 1].address === pair?.asset.address
+
+  const balanceAmount = useBento ? pair?.asset.bentoBalance : assetNative ? info?.ethBalance : pair?.asset.balance
+  const balance =
+    assetToken &&
+    balanceAmount &&
+    CurrencyAmount.fromRawAmount(assetNative ? WNATIVE[chainId || 1] : assetToken, balanceAmount)
+
+  const maxAmount = balance
+
+  const parsedDepositValue = tryParseAmount(depositValue, assetToken)
+
+  const fiatValue = useUSDCValue(parsedDepositValue ?? balance)
+
+  const [kashiApprovalState, approveKashiFallback, kashiPermit, onApproveKashi, onCook] = useKashiApproveCallback()
+
+  const [tokenApprovalState, onApproveToken] = useApproveCallback(
+    parsedDepositValue,
+    chainId && BENTOBOX_ADDRESS[chainId]
+  )
+
+  async function onDeposit(cooker: KashiCooker): Promise<string> {
+    if (pair?.currentExchangeRate.isZero()) {
+      cooker.updateExchangeRate(false, ZERO, ZERO)
+    }
+    cooker.addAsset(BigNumber.from(parsedDepositValue.quotient.toString()), useBento)
+    return `${i18n._(t`Deposit`)} ${pair?.asset.tokenInfo.symbol}`
+  }
+
+  const error = !parsedDepositValue
+    ? 'Enter an amount'
+    : balance.lessThan(parsedDepositValue)
+    ? 'Insufficient balance'
+    : undefined
+
+  const isValid = !error
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <CurrencyInputPanel
+        value={depositValue}
+        currency={assetToken}
+        id="add-liquidity-input-tokenb"
+        onUserInput={(value) => setDepositValue(value)}
+        showMaxButton
+        onMax={() => {
+          setDepositValue(maxAmount?.toExact() ?? '')
+        }}
+        currencyBalance={balance}
+        fiatValue={fiatValue}
+      />
+      {approveKashiFallback && (
+        <Alert
+          message={i18n._(
+            t`Something went wrong during signing of the approval. This is expected for hardware wallets, such as Trezor and Ledger. Click again and the fallback method will be used`
+          )}
+          className="mb-4"
+        />
+      )}
+      {!account ? (
+        <Web3Connect size="lg" color="blue" className="w-full" />
+      ) : isValid &&
+        !kashiPermit &&
+        (kashiApprovalState === BentoApprovalState.NOT_APPROVED ||
+          kashiApprovalState === BentoApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveKashi}
+          disabled={kashiApprovalState !== BentoApprovalState.NOT_APPROVED}
+        >
+          {kashiApprovalState === BentoApprovalState.PENDING ? (
+            <Dots>{i18n._(t`Approving Kashi`)}</Dots>
+          ) : (
+            i18n._(t`Approve Kashi`)
+          )}
+        </Button>
+      ) : isValid &&
+        !useBento &&
+        (tokenApprovalState === ApprovalState.NOT_APPROVED || tokenApprovalState === ApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveToken}
+          disabled={tokenApprovalState !== ApprovalState.NOT_APPROVED}
+        >
+          {tokenApprovalState === ApprovalState.PENDING ? (
+            <Dots>
+              {i18n._(t`Approving`)} {assetToken?.symbol}
+            </Dots>
+          ) : (
+            `${i18n._(t`Approve`)} ${assetToken?.symbol}`
+          )}
+        </Button>
+      ) : (
+        <ButtonError
+          onClick={() => onCook(pair, onDeposit)}
+          disabled={!isValid}
+          error={!isValid && !!parsedDepositValue}
+        >
+          {error || i18n._(t`Confirm Deposit`)}
+        </ButtonError>
+      )}
+    </div>
+  )
+}
+
+KashiDeposit.Provider = Provider
+
+export default KashiDeposit

src/features/onsen/KashiWithdraw.tsx
@@ -1,0 +1,122 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { Currency, CurrencyAmount, currencyEquals, Percent, WNATIVE } from '@sushiswap/sdk'
+import React, { useCallback, useState } from 'react'
+import Alert from '../../components/Alert'
+import Button, { ButtonError } from '../../components/Button'
+import Dots from '../../components/Dots'
+import Web3Connect from '../../components/Web3Connect'
+import { KashiCooker } from '../../entities'
+import { tryParseAmount } from '../../functions'
+import { maximum, minimum, ZERO } from '../../functions/math'
+import { useActiveWeb3React } from '../../hooks'
+import { useCurrency } from '../../hooks/Tokens'
+import useKashiApproveCallback, { BentoApprovalState } from '../../hooks/useKashiApproveCallback'
+import { useUSDCValue } from '../../hooks/useUSDCPrice'
+import { useKashiInfo } from '../kashi/context'
+import CurrencyInputPanel from './CurrencyInputPanel'
+
+const KashiWithdraw = ({ pair, useBento }) => {
+  const { i18n } = useLingui()
+  const { account } = useActiveWeb3React()
+
+  const assetToken = useCurrency(pair?.asset?.address) || undefined
+
+  const [withdrawValue, setWithdrawValue] = useState('')
+
+  const [kashiApprovalState, approveKashiFallback, kashiPermit, onApproveKashi, onCook] = useKashiApproveCallback()
+
+  const amountAvailable = minimum(pair?.maxAssetAvailable ?? ZERO, pair?.currentUserAssetAmount.value ?? ZERO)
+  const available =
+    assetToken && amountAvailable && CurrencyAmount.fromRawAmount(assetToken, amountAvailable.toString())
+
+  const availableFiatValue = useUSDCValue(available)
+
+  const maxAmount = available
+
+  const parsedWithdrawValue = tryParseAmount(withdrawValue, assetToken)
+
+  async function onWithdraw(cooker: KashiCooker): Promise<string> {
+    const maxFraction = minimum(pair.userAssetFraction, pair.maxAssetAvailableFraction)
+    const fraction = BigNumber.from(parsedWithdrawValue.quotient.toString()).mulDiv(
+      pair.currentTotalAsset.base,
+      pair.currentAllAssets.value
+    )
+    console.log('max', maxFraction.toString())
+    console.log(minimum(fraction, maxFraction).toString())
+    cooker.removeAsset(minimum(fraction, maxFraction), useBento)
+    return `${i18n._(t`Withdraw`)} ${pair.asset.tokenInfo.symbol}`
+  }
+
+  const error = !parsedWithdrawValue
+    ? 'Enter an amount'
+    : available?.lessThan(parsedWithdrawValue)
+    ? 'Insufficient balance'
+    : undefined
+
+  const isValid = !error
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <CurrencyInputPanel
+        value={withdrawValue}
+        currency={assetToken}
+        id="add-liquidity-input-tokenb"
+        showMaxButton
+        onMax={() => {
+          setWithdrawValue(maxAmount?.toExact() ?? '')
+        }}
+        onUserInput={(value) => setWithdrawValue(value)}
+        currencyBalance={available}
+        fiatValue={availableFiatValue}
+      />
+      {approveKashiFallback && (
+        <Alert
+          message={i18n._(
+            t`Something went wrong during signing of the approval. This is expected for hardware wallets, such as Trezor and Ledger. Click again and the fallback method will be used`
+          )}
+          className="mb-4"
+        />
+      )}
+      {amountAvailable?.eq(0) && (
+        <Alert
+          message={i18n._(
+            t`Note: If your KMP is staked, you cannot withdraw your lent tokens. You must unstake first.`
+          )}
+          className="mb-4"
+          type="information"
+        />
+      )}
+      {!account ? (
+        <Web3Connect size="lg" color="blue" className="w-full" />
+      ) : isValid &&
+        !kashiPermit &&
+        (kashiApprovalState === BentoApprovalState.NOT_APPROVED ||
+          kashiApprovalState === BentoApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveKashi}
+          disabled={kashiApprovalState !== BentoApprovalState.NOT_APPROVED}
+        >
+          {kashiApprovalState === BentoApprovalState.PENDING ? (
+            <Dots>{i18n._(t`Approving Kashi`)}</Dots>
+          ) : (
+            i18n._(t`Approve Kashi`)
+          )}
+        </Button>
+      ) : (
+        <ButtonError
+          onClick={() => onCook(pair, onWithdraw)}
+          disabled={!isValid}
+          error={!isValid && !!parsedWithdrawValue}
+        >
+          {error || i18n._(t`Confirm Withdraw`)}
+        </ButtonError>
+      )}
+    </div>
+  )
+}
+
+export default KashiWithdraw

src/features/onsen/ManageKashiPair.tsx
@@ -1,0 +1,69 @@
+import { Popover, Switch } from '@headlessui/react'
+import { MinusIcon, PlusIcon } from '@heroicons/react/solid'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import React, { useState } from 'react'
+import { useKashiPair } from '../kashi/context'
+import KashiDeposit from './KashiDeposit'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import KashiWithdraw from './KashiWithdraw'
+
+const ManageKashiPair = ({ farm }) => {
+  const { i18n } = useLingui()
+
+  const kashiPair = useKashiPair(farm.pair.id)
+
+  const [toggle, setToggle] = useState(true)
+  const [useBento, setUseBento] = useState(false)
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <div className="flex items-center justify-between pb-2">
+        <Switch.Group>
+          <div className="flex items-center space-x-4">
+            <Switch
+              checked={toggle}
+              onChange={() => setToggle(!toggle)}
+              className={`${
+                toggle ? 'bg-blue border-blue' : 'bg-pink border-pink'
+              } bg-opacity-60 border border-opacity-80 relative inline-flex items-center h-[32px] rounded-full w-[54px] transition-colors focus:outline-none`}
+            >
+              <span
+                className={`${
+                  toggle ? 'translate-x-[1px] text-blue' : 'translate-x-[23px] text-pink'
+                } inline-block w-7 h-7 transform bg-white rounded-full transition-transform`}
+              >
+                {toggle ? <PlusIcon /> : <MinusIcon />}
+              </span>
+            </Switch>
+            <Switch.Label>{toggle ? i18n._(t`Deposit`) : i18n._(t`Withdraw`)}</Switch.Label>
+            <div className="flex space-x-1">
+              <div className="whitespace-nowrap">{toggle ? i18n._(t`From:`) : i18n._(t`To:`)}</div>
+              <Popover>
+                <Popover.Button>
+                  <div className="flex">
+                    <div>{useBento ? 'BentoBox' : i18n._(t`Wallet`)}</div>
+                    <ChevronDownIcon className="w-3" />
+                  </div>
+                </Popover.Button>
+                <Popover.Panel className="absolute">
+                  <Popover.Button>
+                    <div onClick={() => setUseBento(!useBento)}>{useBento ? i18n._(t`Wallet`) : 'BentoBox'}</div>
+                  </Popover.Button>
+                </Popover.Panel>
+              </Popover>
+            </div>
+          </div>
+        </Switch.Group>
+        <div className="whitespace-nowrap">{toggle ? i18n._(t`Balance`) : i18n._(t`Available to Withdraw`)}</div>
+      </div>
+      {toggle ? (
+        <KashiDeposit pair={kashiPair} useBento={useBento} />
+      ) : (
+        <KashiWithdraw pair={kashiPair} useBento={useBento} />
+      )}
+    </div>
+  )
+}
+
+export default ManageKashiPair

unicorn79dev/sepolia

src/features/farm/KashiDeposit.tsx
@@ -1,0 +1,138 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { BENTOBOX_ADDRESS, CurrencyAmount, WNATIVE } from '@sushiswap/sdk'
+import React, { useState } from 'react'
+import Button, { ButtonError } from '../../components/Button'
+import Dots from '../../components/Dots'
+import Web3Connect from '../../components/Web3Connect'
+import { KashiCooker } from '../../entities'
+import { ZERO } from '../../functions/math'
+import { ApprovalState, useActiveWeb3React, useApproveCallback } from '../../hooks'
+import { useCurrency } from '../../hooks/Tokens'
+import useKashiApproveCallback, { BentoApprovalState } from '../../hooks/useKashiApproveCallback'
+import CurrencyInputPanel from './CurrencyInputPanel'
+import Provider, { useKashiInfo } from '../lending/context'
+import { tryParseAmount } from '../../functions'
+import Alert from '../../components/Alert'
+import { useUSDCValue } from '../../hooks/useUSDCPrice'
+import { BigNumber } from '@ethersproject/bignumber'
+
+const KashiDeposit = ({ pair, useBento }) => {
+  const { i18n } = useLingui()
+  const { account, chainId } = useActiveWeb3React()
+
+  const assetToken = useCurrency(pair?.asset.address) || undefined
+  const info = useKashiInfo()
+
+  const [depositValue, setDepositValue] = useState('')
+
+  const assetNative = WNATIVE[chainId || 1].address === pair?.asset.address
+
+  const balanceAmount = useBento ? pair?.asset.bentoBalance : assetNative ? info?.ethBalance : pair?.asset.balance
+  const balance =
+    assetToken &&
+    balanceAmount &&
+    CurrencyAmount.fromRawAmount(assetNative ? WNATIVE[chainId || 1] : assetToken, balanceAmount)
+
+  const maxAmount = balance
+
+  const parsedDepositValue = tryParseAmount(depositValue, assetToken)
+
+  const fiatValue = useUSDCValue(parsedDepositValue ?? balance)
+
+  const [kashiApprovalState, approveKashiFallback, kashiPermit, onApproveKashi, onCook] = useKashiApproveCallback()
+
+  const [tokenApprovalState, onApproveToken] = useApproveCallback(
+    parsedDepositValue,
+    chainId && BENTOBOX_ADDRESS[chainId]
+  )
+
+  async function onDeposit(cooker: KashiCooker): Promise<string> {
+    if (pair?.currentExchangeRate.isZero()) {
+      cooker.updateExchangeRate(false, ZERO, ZERO)
+    }
+    cooker.addAsset(BigNumber.from(parsedDepositValue.quotient.toString()), useBento)
+    return `${i18n._(t`Deposit`)} ${pair?.asset.tokenInfo.symbol}`
+  }
+
+  const error = !parsedDepositValue
+    ? 'Enter an amount'
+    : balance.lessThan(parsedDepositValue)
+    ? 'Insufficient balance'
+    : undefined
+
+  const isValid = !error
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <CurrencyInputPanel
+        value={depositValue}
+        currency={assetToken}
+        id="add-liquidity-input-tokenb"
+        onUserInput={(value) => setDepositValue(value)}
+        showMaxButton
+        onMax={() => {
+          setDepositValue(maxAmount?.toExact() ?? '')
+        }}
+        currencyBalance={balance}
+        fiatValue={fiatValue}
+      />
+      {approveKashiFallback && (
+        <Alert
+          message={i18n._(
+            t`Something went wrong during signing of the approval. This is expected for hardware wallets, such as Trezor and Ledger. Click again and the fallback method will be used`
+          )}
+          className="mb-4"
+        />
+      )}
+      {!account ? (
+        <Web3Connect size="lg" color="blue" className="w-full" />
+      ) : isValid &&
+        !kashiPermit &&
+        (kashiApprovalState === BentoApprovalState.NOT_APPROVED ||
+          kashiApprovalState === BentoApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveKashi}
+          disabled={kashiApprovalState !== BentoApprovalState.NOT_APPROVED}
+        >
+          {kashiApprovalState === BentoApprovalState.PENDING ? (
+            <Dots>{i18n._(t`Approving Kashi`)}</Dots>
+          ) : (
+            i18n._(t`Approve Kashi`)
+          )}
+        </Button>
+      ) : isValid &&
+        !useBento &&
+        (tokenApprovalState === ApprovalState.NOT_APPROVED || tokenApprovalState === ApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveToken}
+          disabled={tokenApprovalState !== ApprovalState.NOT_APPROVED}
+        >
+          {tokenApprovalState === ApprovalState.PENDING ? (
+            <Dots>
+              {i18n._(t`Approving`)} {assetToken?.symbol}
+            </Dots>
+          ) : (
+            `${i18n._(t`Approve`)} ${assetToken?.symbol}`
+          )}
+        </Button>
+      ) : (
+        <ButtonError
+          onClick={() => onCook(pair, onDeposit)}
+          disabled={!isValid}
+          error={!isValid && !!parsedDepositValue}
+        >
+          {error || i18n._(t`Confirm Deposit`)}
+        </ButtonError>
+      )}
+    </div>
+  )
+}
+
+KashiDeposit.Provider = Provider
+
+export default KashiDeposit

src/features/farm/KashiWithdraw.tsx
@@ -1,0 +1,122 @@
+import { BigNumber } from '@ethersproject/bignumber'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import { Currency, CurrencyAmount, currencyEquals, Percent, WNATIVE } from '@sushiswap/sdk'
+import React, { useCallback, useState } from 'react'
+import Alert from '../../components/Alert'
+import Button, { ButtonError } from '../../components/Button'
+import Dots from '../../components/Dots'
+import Web3Connect from '../../components/Web3Connect'
+import { KashiCooker } from '../../entities'
+import { tryParseAmount } from '../../functions'
+import { maximum, minimum, ZERO } from '../../functions/math'
+import { useActiveWeb3React } from '../../hooks'
+import { useCurrency } from '../../hooks/Tokens'
+import useKashiApproveCallback, { BentoApprovalState } from '../../hooks/useKashiApproveCallback'
+import { useUSDCValue } from '../../hooks/useUSDCPrice'
+import { useKashiInfo } from '../lending/context'
+import CurrencyInputPanel from './CurrencyInputPanel'
+
+const KashiWithdraw = ({ pair, useBento }) => {
+  const { i18n } = useLingui()
+  const { account } = useActiveWeb3React()
+
+  const assetToken = useCurrency(pair?.asset?.address) || undefined
+
+  const [withdrawValue, setWithdrawValue] = useState('')
+
+  const [kashiApprovalState, approveKashiFallback, kashiPermit, onApproveKashi, onCook] = useKashiApproveCallback()
+
+  const amountAvailable = minimum(pair?.maxAssetAvailable ?? ZERO, pair?.currentUserAssetAmount.value ?? ZERO)
+  const available =
+    assetToken && amountAvailable && CurrencyAmount.fromRawAmount(assetToken, amountAvailable.toString())
+
+  const availableFiatValue = useUSDCValue(available)
+
+  const maxAmount = available
+
+  const parsedWithdrawValue = tryParseAmount(withdrawValue, assetToken)
+
+  async function onWithdraw(cooker: KashiCooker): Promise<string> {
+    const maxFraction = minimum(pair.userAssetFraction, pair.maxAssetAvailableFraction)
+    const fraction = BigNumber.from(parsedWithdrawValue.quotient.toString()).mulDiv(
+      pair.currentTotalAsset.base,
+      pair.currentAllAssets.value
+    )
+    console.log('max', maxFraction.toString())
+    console.log(minimum(fraction, maxFraction).toString())
+    cooker.removeAsset(minimum(fraction, maxFraction), useBento)
+    return `${i18n._(t`Withdraw`)} ${pair.asset.tokenInfo.symbol}`
+  }
+
+  const error = !parsedWithdrawValue
+    ? 'Enter an amount'
+    : available?.lessThan(parsedWithdrawValue)
+    ? 'Insufficient balance'
+    : undefined
+
+  const isValid = !error
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <CurrencyInputPanel
+        value={withdrawValue}
+        currency={assetToken}
+        id="add-liquidity-input-tokenb"
+        showMaxButton
+        onMax={() => {
+          setWithdrawValue(maxAmount?.toExact() ?? '')
+        }}
+        onUserInput={(value) => setWithdrawValue(value)}
+        currencyBalance={available}
+        fiatValue={availableFiatValue}
+      />
+      {approveKashiFallback && (
+        <Alert
+          message={i18n._(
+            t`Something went wrong during signing of the approval. This is expected for hardware wallets, such as Trezor and Ledger. Click again and the fallback method will be used`
+          )}
+          className="mb-4"
+        />
+      )}
+      {amountAvailable?.eq(0) && (
+        <Alert
+          message={i18n._(
+            t`Note: If your KMP is staked, you cannot withdraw your lent tokens. You must unstake first.`
+          )}
+          className="mb-4"
+          type="information"
+        />
+      )}
+      {!account ? (
+        <Web3Connect size="lg" color="blue" className="w-full" />
+      ) : isValid &&
+        !kashiPermit &&
+        (kashiApprovalState === BentoApprovalState.NOT_APPROVED ||
+          kashiApprovalState === BentoApprovalState.PENDING) ? (
+        <Button
+          color="gradient"
+          size="lg"
+          onClick={onApproveKashi}
+          disabled={kashiApprovalState !== BentoApprovalState.NOT_APPROVED}
+        >
+          {kashiApprovalState === BentoApprovalState.PENDING ? (
+            <Dots>{i18n._(t`Approving Kashi`)}</Dots>
+          ) : (
+            i18n._(t`Approve Kashi`)
+          )}
+        </Button>
+      ) : (
+        <ButtonError
+          onClick={() => onCook(pair, onWithdraw)}
+          disabled={!isValid}
+          error={!isValid && !!parsedWithdrawValue}
+        >
+          {error || i18n._(t`Confirm Withdraw`)}
+        </ButtonError>
+      )}
+    </div>
+  )
+}
+
+export default KashiWithdraw

src/features/farm/ManageKashiPair.tsx
@@ -1,0 +1,69 @@
+import { Popover, Switch } from '@headlessui/react'
+import { MinusIcon, PlusIcon } from '@heroicons/react/solid'
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import React, { useState } from 'react'
+import { useKashiPair } from '../lending/context'
+import KashiDeposit from './KashiDeposit'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import KashiWithdraw from './KashiWithdraw'
+
+const ManageKashiPair = ({ farm }) => {
+  const { i18n } = useLingui()
+
+  const kashiPair = useKashiPair(farm.pair.id)
+
+  const [toggle, setToggle] = useState(true)
+  const [useBento, setUseBento] = useState(false)
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <div className="flex items-center justify-between pb-2">
+        <Switch.Group>
+          <div className="flex items-center space-x-4">
+            <Switch
+              checked={toggle}
+              onChange={() => setToggle(!toggle)}
+              className={`${
+                toggle ? 'bg-blue border-blue' : 'bg-pink border-pink'
+              } bg-opacity-60 border border-opacity-80 relative inline-flex items-center h-[32px] rounded-full w-[54px] transition-colors focus:outline-none`}
+            >
+              <span
+                className={`${
+                  toggle ? 'translate-x-[1px] text-blue' : 'translate-x-[23px] text-pink'
+                } inline-block w-7 h-7 transform bg-white rounded-full transition-transform`}
+              >
+                {toggle ? <PlusIcon /> : <MinusIcon />}
+              </span>
+            </Switch>
+            <Switch.Label>{toggle ? i18n._(t`Deposit`) : i18n._(t`Withdraw`)}</Switch.Label>
+            <div className="flex space-x-1">
+              <div className="whitespace-nowrap">{toggle ? i18n._(t`From:`) : i18n._(t`To:`)}</div>
+              <Popover>
+                <Popover.Button>
+                  <div className="flex">
+                    <div>{useBento ? 'BentoBox' : i18n._(t`Wallet`)}</div>
+                    <ChevronDownIcon className="w-3" />
+                  </div>
+                </Popover.Button>
+                <Popover.Panel className="absolute">
+                  <Popover.Button>
+                    <div onClick={() => setUseBento(!useBento)}>{useBento ? i18n._(t`Wallet`) : 'BentoBox'}</div>
+                  </Popover.Button>
+                </Popover.Panel>
+              </Popover>
+            </div>
+          </div>
+        </Switch.Group>
+        <div className="whitespace-nowrap">{toggle ? i18n._(t`Balance`) : i18n._(t`Available to Withdraw`)}</div>
+      </div>
+      {toggle ? (
+        <KashiDeposit pair={kashiPair} useBento={useBento} />
+      ) : (
+        <KashiWithdraw pair={kashiPair} useBento={useBento} />
+      )}
+    </div>
+  )
+}
+
+export default ManageKashiPair